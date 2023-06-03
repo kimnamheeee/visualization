f5a6f82d6633052136f16feaff1352f7aeb26b35
Kim Nam Hee <103100783+kimnamheeee@users.noreply.github.com>
2023-06-03 15:58:13 +0900

    feat: change main page composition

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { MainPage1, MainPage2 } from "./components/MainPage/Index";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [scrollAttempts, setScrollAttempts] = useState(0);
+  const [showMainPage2, setShowMainPage2] = useState(false);
+
+  const handleWheel = (event) => {
+    const deltaY = event.deltaY;
+    if (deltaY > 10) {
+      console.log("scroll attempt");
+      setScrollAttempts((prevAttempts) => prevAttempts + 1);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("wheel", handleWheel);
+
+    return () => {
+      window.removeEventListener("wheel", handleWheel);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (scrollAttempts > 20) {
+      setShowMainPage2(true);
+    }
+  }, [scrollAttempts]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App">{showMainPage2 ? <MainPage2 /> : <MainPage1 />}</div>
   );
 }
 
