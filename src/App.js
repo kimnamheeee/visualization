7e0746612fe9e03fa66e91d6afea20485372efe5
Kim Nam Hee <103100783+kimnamheeee@users.noreply.github.com>
2023-06-14 00:23:30 +0900

    react-fullpage 라이브러리 사용하게 변경 (미완)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
-// import { MainPage1, MainPage2, MainPage4 } from "./components/MainPage/Index";
-import { useState } from "react";
+import {
+  MainPage1,
+  MainPage2,
+  MainPage3,
+  MainPage4,
+} from "./components/MainPage/Index";
+import { useState, useEffect, useCallback } from "react";
 import {
   RadarChart2018,
   RadarChart2019,
@@ -10,19 +15,23 @@ import {
 } from "./components/RadarChart/Index";
 import YearBar from "./components/YearBar/Index";
 import { Modal } from "./components/Modal/Index";
+import { SectionsContainer, Section } from "react-fullpage";
 
 function App() {
   // const [scrollAttempts, setScrollAttempts] = useState(0);
   // const [showMainPage, setShowMainPage] = useState(1);
 
-  // const handleWheel = (event) => {
-  //   const deltaY = event.deltaY;
-  //   if (deltaY > 10) {
-  //     setScrollAttempts((prevAttempts) => prevAttempts + 1);
-  //   } else if (deltaY < -10 && scrollAttempts > 0) {
-  //     setScrollAttempts((prevAttempts) => prevAttempts - 1);
-  //   }
-  // };
+  // const handleWheel = useCallback(
+  //   throttle((event) => {
+  //     const deltaY = event.deltaY;
+  //     if (deltaY > 10) {
+  //       setScrollAttempts((prevAttempts) => prevAttempts + 1);
+  //     } else if (deltaY < -10 && scrollAttempts > 0) {
+  //       setScrollAttempts((prevAttempts) => prevAttempts - 1);
+  //     }
+  //   }, 500),
+  //   [scrollAttempts]
+  // );
 
   // useEffect(() => {
   //   window.addEventListener("wheel", handleWheel);
@@ -34,14 +43,32 @@ function App() {
 
   // useEffect(() => {
   //   console.log("attempt", scrollAttempts);
-  //   if (scrollAttempts >= 0 && scrollAttempts <= 20) {
+  //   if (scrollAttempts >= 0 && scrollAttempts <= 10) {
   //     setShowMainPage(1);
-  //   } else if (scrollAttempts > 20 && scrollAttempts <= 40) {
+  //   } else if (scrollAttempts > 10 && scrollAttempts <= 35) {
   //     setShowMainPage(2);
-  //   } else if (scrollAttempts > 40 && scrollAttempts <= 60) {
+  //   } else if (scrollAttempts > 35 && scrollAttempts <= 55) {
+  //     setShowMainPage(3);
+  //   } else if (scrollAttempts > 55 && scrollAttempts <= 80) {
   //     setShowMainPage(4);
+  //   } else if (scrollAttempts > 80 && scrollAttempts <= 100) {
+  //     setShowMainPage(5);
+  //   } else {
+  //     setScrollAttempts(100);
   //   }
   // }, [scrollAttempts]);
+
+  const options = {
+    anchors: [
+      "mainPage1",
+      "mainPage2",
+      "mainPage3",
+      "mainPage4",
+      "radarcharts",
+    ],
+    scrollBar: false,
+    navigation: true,
+  };
 
   const [selectedCircle, setSelectedCircle] = useState(0);
   const [hoveredChart, setHoveredChart] = useState({
@@ -118,23 +145,36 @@ function App() {
 
   return (
     <div className="App">
-      {/* {showMainPage === 1 && <MainPage1 />}
-      {showMainPage === 2 && <MainPage2 />}
-      {showMainPage === 4 && <MainPage4 />} */}
-      {showModal && (
-        <Modal
-          region={selectedRegion.region}
-          year={selectedRegion.year}
-          setShowModal={setShowModal}
-        />
-      )}
-      {radarChart}
-      <div className="main-yearbar">
-        <YearBar
-          selectedCircle={selectedCircle}
-          setSelectedCircle={setSelectedCircle}
-        />
-      </div>
+      <SectionsContainer {...options}>
+        <Section>
+          <MainPage1 />
+        </Section>
+        <Section>
+          <MainPage2 />
+        </Section>
+        <Section>
+          <MainPage3 />
+        </Section>
+        <Section>
+          <MainPage4 />
+        </Section>
+        <Section>
+          {showModal && (
+            <Modal
+              region={selectedRegion.region}
+              year={selectedRegion.year}
+              setShowModal={setShowModal}
+            />
+          )}
+          {radarChart}
+          <div className="main-yearbar">
+            <YearBar
+              selectedCircle={selectedCircle}
+              setSelectedCircle={setSelectedCircle}
+            />
+          </div>
+        </Section>
+      </SectionsContainer>
     </div>
   );
 }
