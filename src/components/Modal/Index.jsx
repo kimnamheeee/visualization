783a6a3ebdf3b798b472523b602e13fc3bc3164b
Kim Nam Hee <103100783+kimnamheeee@users.noreply.github.com>
2023-06-14 16:18:17 +0900

    모달 창 1페이지에 json 데이터 적용

--- src/components/Modal/Index.jsx
+++ src/components/Modal/Index.jsx
@@ -9,6 +9,8 @@ import {
 import close from "../../assets/images/close.svg";
 import radarDescription from "../../assets/images/radar-description.svg";
 import Select from "react-select";
+import datas from "../../data/datas.json";
+import { useState } from "react";
 
 export const Modal = ({ region, year, setShowModal }) => {
   const region_names = [
@@ -57,6 +59,24 @@ export const Modal = ({ region, year, setShowModal }) => {
   }));
 
   const years = [2018, 2019, 2020, 2021, 2022];
+
+  const [totalValue, setTotalValue] = useState(
+    datas[year][region_names_kor[region]]["예산"]["total"] * 1000000
+  );
+
+  const [yearRegionBudget, setYearRegionBudget] = useState(
+    datas[year][region_names_kor[region]]["예산"]
+  );
+
+  const { total, ...budgetValues } = yearRegionBudget;
+
+  const maxBudget = Math.max(...Object.values(budgetValues));
+  const maxBudgetKey = Object.keys(budgetValues).find(
+    (key) => budgetValues[key] === maxBudget
+  );
+
+  console.log("max", maxBudget);
+  console.log("key", maxBudgetKey);
 
   const yearOptions = years.map((year) => ({
     value: year,
@@ -109,6 +129,12 @@ export const Modal = ({ region, year, setShowModal }) => {
     }),
   };
 
+  const defaultYearOption = { value: year, label: year };
+  const defaultRegionOption = {
+    value: region_names_kor[region],
+    label: region_names_kor[region],
+  };
+
   return (
     <div className="modal-background">
       <div className="modal-container">
@@ -124,17 +150,22 @@ export const Modal = ({ region, year, setShowModal }) => {
               components={customComponents}
               styles={customSelectStyles}
               options={yearOptions}
+              defaultValue={defaultYearOption}
             ></Select>
             <Select
               components={customComponents}
               styles={customSelectStyles}
               options={regionOptions}
               maxMenuHeight={500}
+              defaultValue={defaultRegionOption}
             />
           </div>
           <div className="totalnumber-container text">
-            {region_names_kor[region]}은(는) {year}년 저출산 정책의 예산으로 총
-            원을 지출하였습니다.
+            {region_names_kor[region]}은(는) {year}년 저출산 정책의 예산으로 총{" "}
+            <span style={{ color: "#54BFCF" }}>
+              {totalValue.toLocaleString()}
+            </span>
+            원을 책정하였습니다.
           </div>
           <div className="charts-container">
             <div className="radar">
@@ -151,10 +182,14 @@ export const Modal = ({ region, year, setShowModal }) => {
             </div>
           </div>
           <div className="explanation-container">
-            <div className="text">
-              그 중 가장 많은 예산을 지출한 단계는 ''로, 총 원을 지출하였습니다.
+            <div className="text explanation">
+              그 중 가장 많은 예산이 편성된 단계는 {maxBudgetKey}(으)로, 총{" "}
+              <span style={{ color: "#54BFCF" }}>
+                {(maxBudget * 1000000).toLocaleString()}
+              </span>
+              원이 책정되었습니다.
             </div>
-            <div className="text">
+            <div className="text explanation">
               어떠한 정책들이 있었는지 자세히 알아볼까요?
             </div>
           </div>
